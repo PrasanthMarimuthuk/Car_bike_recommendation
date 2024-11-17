d43d8dd2c5d789327f8fd5b5d5db64453a461bc3
PrasanthMarimuthuk <93981833+PrasanthMarimuthuk@users.noreply.github.com>
2024-11-17 12:53:20 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -12,10 +12,15 @@ bike_data = pd.read_csv('bikesCleaned.csv')
 car_brands = car_data['Make'].unique().tolist()
 bike_brands = bike_data['Brand'].unique().tolist()
 
-
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    previous_choice = 'car'  # Default to 'car' if no prior selection
+    previous_brand = None
+
     if request.method == 'POST':
+        previous_choice = request.form.get('choice')
+        previous_brand = request.form.get('brand_name')
+
         choice = request.form.get('choice')
         brand_name = request.form.get('brand_name')
         price_range = request.form.get('price_range', '0,1000000').split(',')
@@ -30,29 +35,37 @@ def index():
             recommended_cars = car_data[(car_data['Make'] == brand_name) & 
                                         (car_data['Price'] >= price_range[0]) & 
                                         (car_data['Price'] <= price_range[1])]
-            recommended_cars = recommended_cars.dropna()  # Remove rows with NaN values
             if location != "All Locations":
                 recommended_cars = recommended_cars[recommended_cars['Location'] == location]
             if filter == 'low_price':
                 recommended_cars = recommended_cars.sort_values(by='Price')
             elif filter == 'high_mileage':
                 recommended_cars = recommended_cars.sort_values(by='Kilometer', ascending=False)
-            return render_template('results.html', results=recommended_cars.head(5).to_dict('records'), all_results=recommended_cars.to_dict('records'), type='cars')
+            return render_template('results.html', 
+                                   results=recommended_cars.head(5).fillna('-').to_dict('records'), 
+                                   all_results=recommended_cars.fillna('-').to_dict('records'), 
+                                   type='cars')
+
         elif choice == 'bike':
             recommended_bikes = bike_data[(bike_data['Brand'] == brand_name) & 
                                           (bike_data['Price'] >= price_range[0]) & 
                                           (bike_data['Price'] <= price_range[1])]
-            recommended_bikes = recommended_bikes.dropna()  # Remove rows with NaN values
             if location != "All Locations":
                 recommended_bikes = recommended_bikes[recommended_bikes['Location'] == location]
-            recommended_bikes = recommended_bikes[['Brand','Model','Price','Max power','Max torque','Cooling System', 'Transmission','Fuel Tank Capacity','Location','Mileage','Top speed','Kerb weight','Kilometer Driven']]
             if filter == 'low_price':
                 recommended_bikes = recommended_bikes.sort_values(by='Price')
             elif filter == 'high_mileage':
                 recommended_bikes = recommended_bikes.sort_values(by='Mileage', ascending=False)
-            return render_template('results.html', results=recommended_bikes.head(5).to_dict('records'), all_results=recommended_bikes.to_dict('records'), type='bikes')
-    return render_template('index.html', car_brands=car_brands, bike_brands=bike_brands)
+            return render_template('results.html', 
+                                   results=recommended_bikes.head(5).fillna('-').to_dict('records'), 
+                                   all_results=recommended_bikes.fillna('-').to_dict('records'), 
+                                   type='bikes')
 
+    return render_template('index.html', 
+                           car_brands=car_brands, 
+                           bike_brands=bike_brands, 
+                           previous_choice=previous_choice, 
+                           previous_brand=previous_brand)
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(debug=True)
